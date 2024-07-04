835e902dced5ecadb2a73082d6f986ff75090ad7
SebastianHerreraPC <sebehepe00@gmail.com>
2024-07-04 13:45:23 -0600

    scripts iniciales

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,11 @@
 //seleccion
 document.addEventListener("DOMContentLoaded", function () {
+  const editButton = querySelector(".button__type-edit");
+  const closeButton = querySelector(".button__edit-close");
   const profileName = querySelector(".profile__name-text");
   const profileJob = querySelector(".profile__job");
-  const inputName = this.querySelector("#popup-Nombre");
-  const inputJob = this.querySelector("#popup-Trabajo");
+  const inputName = querySelector(".popup__input-name");
+  const inputJob = querySelector(".popup__input-job");
+  const popEdit = querySelector(".popup__forms");
+  const popProfile = querySelector(".popup__edit-profile");
 });
