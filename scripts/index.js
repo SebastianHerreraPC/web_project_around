08dc6018d256fc153bb04474e7e86b993634af6f
SebastianHerreraPc <sebehepe00@gmail.com>
2024-07-03 23:14:48 -0600

    primeras variables

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+//seleccion
+document.addEventListener("DOMContentLoaded", function () {
+  const profileName = querySelector(".profile__name-text");
+  const profileJob = querySelector(".profile__job");
+  const inputName = this.querySelector("#popup-Nombre");
+  const inputJob = this.querySelector("#popup-Trabajo");
+});
