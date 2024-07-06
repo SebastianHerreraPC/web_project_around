cb252a99145cf9a27938db5e7638337664505e7c
SebastianHerreraPc <sebehepe00@gmail.com>
2024-07-05 23:07:40 -0600

    funcion display

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,30 @@
 //seleccion
 document.addEventListener("DOMContentLoaded", function () {
-  const editButton = querySelector(".button__type-edit");
-  const closeButton = querySelector(".button__edit-close");
-  const profileName = querySelector(".profile__name-text");
-  const profileJob = querySelector(".profile__job");
-  const inputName = querySelector(".popup__input-name");
-  const inputJob = querySelector(".popup__input-job");
-  const popEdit = querySelector(".popup__forms");
-  const popProfile = querySelector(".popup__edit-profile");
+  const editButton = document.querySelector(".button__type-edit");
+  const closeButton = document.querySelector(".button__edit-close");
+  const profileName = document.querySelector(".profile__name-text");
+  const profileJob = document.querySelector(".profile__job");
+  const inputName = document.querySelector(".popup__input-name");
+  const inputJob = document.querySelector(".popup__input-job");
+  const popEdit = document.querySelector(".popup__form");
+  const popProfile = document.querySelector(".popup__edit-profile");
 
   //primera funcion para que aparezca
   function popupOpen() {
-    popProfile.style.display("block");
+    popProfile.style.display = "block";
   }
 
   //funcion  para que se oculte
   function popupClose() {
-    popProfile.style.display("none");
+    popProfile.style.display = "none";
   }
   //funcion de el boton
+  editButton.addEventListener("click", function () {
+    popupOpen();
+    inputName.value = profileName.textContent;
+    inputJob.value = profileJob.textContent;
+  });
+  closeButton.addEventListener("click", function () {
+    popupClose();
+  });
 });
