02c3d18131941f4cade36dd4a3bbf396730a1c5e
SebastianHerreraPc <sebehepe00@gmail.com>
2024-07-06 23:19:42 -0600

    botones  de agregar

--- scripts/index2.js
+++ scripts/index2.js
@@ -2,3 +2,18 @@
 
 const addPhoto = document.querySelector(".button__type-add");
 const popAdd = document.querySelector(".popup__add-images");
+
+//funcion de abrir
+
+function popupOpenEdit() {
+  popAdd.style.display = "block";
+}
+//funcion cerrar
+function popupCloseEdit() {
+  popAdd.style.display = "none";
+}
+
+//funcion de boton
+addPhoto.addEventListener("click", function () {
+  popupOpenEdit();
+});
