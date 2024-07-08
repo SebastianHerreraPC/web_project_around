c3f56192d8fcb1938990f5b3e58c028a1858fc0b
SebastianHerreraPC <sebehepe00@gmail.com>
2024-07-08 13:21:35 -0600

    initial

--- scripts/index2.js
+++ scripts/index2.js
@@ -7,6 +7,32 @@ document.addEventListener("DOMContentLoaded", function () {
   const popImput = document.querySelector(".popup__lugar");
   const popTemplate = document.querySelector("#card__template");
   const cardsArea = document.querySelector(".cards__container");
+  const initialCards = [
+    {
+      name: "Valle de Yosemite",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+    },
+    {
+      name: "Lago Louise",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+    },
+    {
+      name: "Montañas Calvas",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+    },
+    {
+      name: "Latemar",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+    },
+    {
+      name: "Parque Nacional de la Vanoise",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+    },
+    {
+      name: "Lago di Braies",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+    },
+  ];
 
   //funcionalidades dentro de el boton
   function popupAddOpen() {
