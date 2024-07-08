f4587e00dd944bdac754e15abadd2cbbb222d445
SebastianHerreraPc <sebehepe00@gmail.com>
2024-07-07 22:56:00 -0600

    nuevo dom

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 //seleccion
 document.addEventListener("DOMContentLoaded", function () {
-  const editButton = document.querySelector(".button__type-edit");
-  const closeButton = document.querySelector(".button__edit-close");
+  const editButton = document.querySelector(".profile__button_edit");
+  const closeButton = document.querySelector(".popup__edit_close");
   const profileName = document.querySelector(".profile__name-text");
   const profileJob = document.querySelector(".profile__job");
   const inputName = document.querySelector(".popup__input-name");
