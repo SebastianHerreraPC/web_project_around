03eab2f6501a1df62890f0eeae7a1190e0afbf34
SebastianHerreraPc <sebehepe00@gmail.com>
2024-07-09 20:41:36 -0600

    popup__edit_close

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 //seleccion
 document.addEventListener("DOMContentLoaded", function () {
   const editButton = document.querySelector(".profile__button_edit");
-  const closeButton = document.querySelector(".popup__content-edit_close");
+  const closeButton = document.querySelector(".popup__close--edit");
   const profileName = document.querySelector(".profile__name-text");
   const profileJob = document.querySelector(".profile__job");
   const inputName = document.querySelector(".popup__input-name");
